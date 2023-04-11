4265c76606c6f6dd56f857dafa5cb56b6df34134
jerome.sigg <jerome.sigg@gmail.com>
2023-04-11 19:34:19 +0200

    Reihenfolge in Event Übersicht geändert

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -8,91 +8,93 @@
             <h3>{{$title}}</h3>
         </header>
 
-            <div id="filter_btns">
-                <div id="date_btn">
-                    <div class="row" style="width: 20%">
-                        <div class="col-md-6">
-                             <button class="btn btn-primary">Alle</button>
-                        </div>
-                        <div class="col-md-6">
-                            <button class="btn btn-primary active">Ab Heute</button>
-                        </div>
+        <div id="filter_btns">
+            <div id="date_btn">
+                <div class="row" style="width: 20%">
+                    <div class="col-md-6">
+                         <button class="btn btn-primary">Alle</button>
                     </div>
-                </div>
-                <div id="status_btn">
-                    <br>
-                    <div class="row">
-                        <div class="col-md-1">
-                            <button class="btn btn-secondary active">Alle</button>
-                        </div>
-                        @foreach ($contract_statuses as $contract_status)
-                            <div class="col-md-1">
-                                <button class="btn btn-secondary">{{$contract_status}}</button>
-                            </div>
-                        @endforeach
+                    <div class="col-md-6">
+                        <button class="btn btn-primary active">Ab Heute</button>
                     </div>
                 </div>
             </div>
-            <input type="hidden" value="Ab Heute" id="date_btn_value">
-            <input type="hidden" value="Alle" id="status_btn_value">
-            <br>
-            <table class="table table-striped table-bordered" style="width:100%" id="datatable">
-                <thead>
-                    <tr>
-                        <th scope="col" width="8%">Datum</th>
-                        <th scope="col" width="7%">Nr.</th>
-                        <th scope="col" width="10%">Name</th>
-                        <th scope="col" width="15%">E-Mail</th>
-                        <th scope="col" width="5%">Total</th>
-                        <th scope="col" width="25%">Bemerkung</th>
-                        <th scope="col" width="15%">Bemerkung Intern</th>
-                        <th scope="col" width="15%">Status</th>
-                    </tr>
-                </thead>
-            </table>
-            <div class="hk-reservation hk-reservation__step1">
-                <div class="hk-reservation__container container-fluid">
-                    <div class="row">
-                        <div class="col-lg-12 hk-calendar">
-                            <div class="hk-agenda">
-                                <div class="d-none d-sm-block">
-                                    <a class="hk-agenda__prev" onclick="Agenda.prev(3); return false" href="#">
+            <div id="status_btn">
+                <br>
+                <div class="row">
+                    <div class="col-md-1">
+                        <button class="btn btn-secondary active">Alle</button>
+                    </div>
+                    @foreach ($contract_statuses as $contract_status)
+                        <div class="col-md-1">
+                            <button class="btn btn-secondary">{{$contract_status}}</button>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+        <input type="hidden" value="Ab Heute" id="date_btn_value">
+        <input type="hidden" value="Alle" id="status_btn_value">
+        <br>
+        <div class="hk-reservation hk-reservation__step1">
+            <div class="hk-reservation__container container-fluid">
+                <div class="row">
+                    <div class="col-lg-12 hk-calendar">
+                        <div class="hk-agenda">
+                            <div class="d-none d-sm-block">
+                                <a class="hk-agenda__prev" onclick="Agenda.prev(3); return false" href="#">
+                                    früheres Datum
+                                </a>
+                            </div>
+                            <div class="row">
+                                @for ($i = 0; $i <= 9; $i++)
+                                    <div class="col-md-4 col-sm-6 {{($i>1)?'d-none d-sm-block':''}}">
+                                        <h4 id="agendaTitel{{$i}}" class="hk-agenda__title"> </h4>
+                                        <table class="hk-agenda__month">
+                                            <tbody id="agendaMonat{{$i}}"> </tbody>
+                                        </table>
+                                    </div>
+                                @endfor
+                            </div>
+                            <div class="d-none d-sm-block">
+                                <a class="hk-agenda__next" onclick="Agenda.next(3); return false" href="#">
+                                    späteres Datum
+                                </a>
+                            </div>
+                            <!-- mobile buttons -->
+                            <div class="row d-flex d-sm-none">
+                                <div class="col-6">
+                                    <a class="hk-agenda__prev hk-agenda__prev--mobile" onclick="Agenda.prev(1); return false" href="#">
                                         früheres Datum
                                     </a>
                                 </div>
-                                <div class="row">
-                                    @for ($i = 0; $i <= 9; $i++)
-                                        <div class="col-md-4 col-sm-6 {{($i>1)?'d-none d-sm-block':''}}">
-                                            <h4 id="agendaTitel{{$i}}" class="hk-agenda__title"> </h4>
-                                            <table class="hk-agenda__month">
-                                                <tbody id="agendaMonat{{$i}}"> </tbody>
-                                            </table>
-                                        </div>
-                                    @endfor
-                                </div>
-                                <div class="d-none d-sm-block">
-                                    <a class="hk-agenda__next" onclick="Agenda.next(3); return false" href="#">
+                                <div class="col-6 text-right">
+                                    <a class="hk-agenda__next hk-agenda__next--mobile" onclick="Agenda.next(1); return false" href="#">
                                         späteres Datum
                                     </a>
-                                </div>
-                                <!-- mobile buttons -->
-                                <div class="row d-flex d-sm-none">
-                                    <div class="col-6">
-                                        <a class="hk-agenda__prev hk-agenda__prev--mobile" onclick="Agenda.prev(1); return false" href="#">
-                                            früheres Datum
-                                        </a>
-                                    </div>
-                                    <div class="col-6 text-right">
-                                        <a class="hk-agenda__next hk-agenda__next--mobile" onclick="Agenda.next(1); return false" href="#">
-                                            späteres Datum
-                                        </a>
-                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
+        <br>
+        <table class="table table-striped table-bordered" style="width:100%" id="datatable">
+            <thead>
+                <tr>
+                    <th scope="col" width="8%">Datum</th>
+                    <th scope="col" width="7%">Nr.</th>
+                    <th scope="col" width="10%">Name</th>
+                    <th scope="col" width="15%">E-Mail</th>
+                    <th scope="col" width="5%">Total</th>
+                    <th scope="col" width="25%">Bemerkung</th>
+                    <th scope="col" width="15%">Bemerkung Intern</th>
+                    <th scope="col" width="15%">Status</th>
+                </tr>
+            </thead>
+        </table>
+
     </div>
 </section>
 @endsection
