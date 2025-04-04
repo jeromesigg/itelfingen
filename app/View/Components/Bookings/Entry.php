c972cb81f5248439c38b1f950ace3ab124c47300
jerome.sigg <jerome.sigg+newsletter@gmail.com>
2025-04-04 22:00:49 +0200

    Parkkarte in Kundenlogin

--- app/View/Components/Bookings/Entry.php
+++ app/View/Components/Bookings/Entry.php
@@ -91,16 +91,22 @@ class Entry extends Component
                         break;
                     }
                 }
-                $this->contents = [[
-                    'link'  => route('bookings.downloadLastInfos', $event['uuid']),
-                    'text'  => 'Letzte Infos ansehen',
-                ],
-                [
-                    'link'  =>'#',
-                    'text'  => 'Türcode: ' . $event['code'],
-                    'path' => 'M10 14v3m4-6V7a3 3 0 1 1 6 0v4M5 11h10a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-7a1 1 0 0 1 1-1Z',      
-                ]];
-                
+                $this->contents = [
+                    [
+                        'link' => route('bookings.downloadLastInfos', $event['uuid']),
+                        'text' => 'Letzte Infos ansehen',
+                    ],
+                    [
+                        'link' =>'#',
+                        'text' => 'Türcode: ' . $event['code'],
+                        'path' => 'M10 14v3m4-6V7a3 3 0 1 1 6 0v4M5 11h10a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-7a1 1 0 0 1 1-1Z',      
+                    ],
+                    [
+                        'link' => route('events.downloadParking', $event),
+                        'text' => 'Parkplatz-Karte herunterladen',
+                        'path' => 'M5 17v-5h1.5a1.5 1.5 0 1 1 0 3H5m12 2v-5h2m-2 3h2M5 10V7.914a1 1 0 0 1 .293-.707l3.914-3.914A1 1 0 0 1 9.914 3H18a1 1 0 0 1 1 1v6M5 19v1a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-1M10 3v4a1 1 0 0 1-1 1H5m6 4v5h1.375A1.627 1.627 0 0 0 14 15.375v-1.75A1.627 1.627 0 0 0 12.375 12H11Z',
+                    ],
+                ];
                 break;
             case 'EventStay':
                 $this->fulfilled = $event['last_info'] && ($event['start_date'] <= now());
