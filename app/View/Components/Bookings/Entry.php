a5e28e49610d46cd88155755d2cc16db946567c4
Jerome Sigg <jeromesigg@gmail.com>
2026-06-30 19:33:48 +0000

    Korrektur in Kundenlogin bei Rechnungsanzeige

--- app/View/Components/Bookings/Entry.php
+++ app/View/Components/Bookings/Entry.php
@@ -150,7 +150,7 @@ class Entry extends Component
                     'path' => 'M9.529 9.988a2.502 2.502 0 1 1 5 .191A2.441 2.441 0 0 1 12 12.582V14m-.01 3.008H12M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z',
                 ]];
                 
-                if(isset($offer['network_link'])){
+                if(isset($invoice['network_link'])){
                     array_push($this->contents,
                     [
                         'link'  => $invoice['network_link'],
