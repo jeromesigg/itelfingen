4e401a6a6d1732822161962983abec8c784797fa
Jerome Sigg <jeromesigg@gmail.com>
2026-07-06 17:36:07 +0000

    Event ID Anzeige bei Edit

--- app/Http/Controllers/AdminEventController.php
+++ app/Http/Controllers/AdminEventController.php
@@ -192,7 +192,7 @@ class AdminEventController extends Controller
         $contract_statuses = ContractStatus::pluck('name', 'id')->all();
         $event = Event::findOrFail($id);
         $users = User::where('role_id', config('status.role_Verwalter'))->pluck('username', 'id')->all();
-        $title = 'Buchung bearbeiten';
+        $title = str_pad($event['id'], 5, '0', STR_PAD_LEFT) . ' - Buchung bearbeiten';
 
         return view('admin.events.edit', compact('event_statuses', 'event', 'contract_statuses', 'users', 'positions', 'title'));
     }
